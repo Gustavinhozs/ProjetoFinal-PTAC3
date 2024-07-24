e63588f6d7081081a9d98d83dd69d9a0de1c9c68
the real px <gustagollindao14@gmail.com>
2024-07-24 11:46:15 -0300

    modificações page.js
    modificações page.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -6,70 +6,80 @@ export const listaDeCarros = [
         modelo: "Golf GTI",
         fabricante: "VW",
         ano: 2018,
-        cor: "Vermelho"
+        cor: "Vermelho",
+        linkImg:"https://i.ytimg.com/vi/amdAArbiMtw/maxresdefault.jpg"
     },
     {
         id: 2,
         modelo: "Civic",
         fabricante: "Honda",
         ano: 2020,
-        cor: "prata"
+        cor: "prata",
+        linkImg: "https://cdn.motor1.com/images/mgl/KqZVA/s3/comparativo-novo-corolla-x-civic-x-cruze-2020.jpg"
     },
     {
         id: 3,
         modelo: "Zafira",
         fabricante: "Chevrolet",
         ano: 2012,
-        cor: "preta"
+        cor: "preta",
+        linkImg: "https://s2-autoesporte.glbimg.com/cYK8l7P3NnoU2YSky3ZIWfmx7tM=/0x0:620x465/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/F/i/OavQp0RG6KKeIp8ZDfog/2016-04-12-chevrolet-zafira.jpg"
     },
     {
         id: 4,
         modelo: "Astra",
         fabricante: "Opel",
         ano: 2020,
-        cor: "amarelo"
+        cor: "amarelo",
+        linkImg: "https://quatrorodas.abril.com.br/wp-content/uploads/2021/07/Opel-Astra-2022-2.jpg?quality=70&strip=info&w=720&crop=1"
     },
     {
         id: 5,
         modelo: "Audi a3",
         fabricante: "Audi",
         ano: 2020,
-        cor: "verde"
+        cor: "Preto",
+        linkImg: "https://cdn.motor1.com/images/mgl/pPpzP/s1/2021-audi-a3-sedan-debut.jpg",
     },
     {
         id: 6,
         modelo: "Mercedes-Benz Gt-r",
         fabricante: "Mercedes-Benz",
         ano: 2020,
-        cor: "Branco"
+        cor: "Roxa",
+        linkImg:"https://pbs.twimg.com/media/F99NeR6WEAAEb59?format=jpg&name=large"
     },
     {
         id: 7,
         modelo: "BMW M4 Competition",
         fabricante: "BMW",
         ano: 2024,
-        cor: "Vermelho"
+        cor: "Vermelho",
+        linkImg: "https://p2.trrsf.com/image/fget/cf/1200/630/middle/images.terra.com/2020/10/23/p90403310lowresbmw-m4-design-study.jpg"
     },
     {
         id: 8,
         modelo: "Porsche 911 Carrera S",
         fabricante: "Porsche",
         ano: 2022,
-        cor: "Prata"
+        cor: "Vermelho",
+        linkImg: "https://static.wixstatic.com/media/ce3e3a_cf871bc9018a4e6bbbbda617786440bb~mv2.jpg/v1/fill/w_640,h_354,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/ce3e3a_cf871bc9018a4e6bbbbda617786440bb~mv2.jpg"
     },
     {
         id: 9,
         modelo: "Porsche Panamera",
         fabricante: "Porsche",
         ano: 2022,
-        cor: "Preto"
+        cor: "Preto",
+        linkImg: "https://presskit.porsche.de/models/assets/images/7/Slider_SEHybr_02_summary-98ce0cb7.jpg"
     },  
       {
         id: 10,
         modelo: "Mercedes G63 ",
         fabricante: "Mercedes-Benz",
         ano: 2022,
-        cor: "Branco"
+        cor: "Preto",
+        linkImg: "https://quatrorodas.abril.com.br/wp-content/uploads/2020/08/Inkas-Mercedes-AMG-G63-Limo-8-e1597943948778.jpg?quality=70&strip=info&w=720&crop=1   "
     },
 ];
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,60 @@
+'use client'
 import Image from "next/image";
 import "./styles.css";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Home() {
 
-  return (
-    <main>
-     
-    </main>
-  );
+  const [listaDeCarros, setListaDeCarros] = useState([]);
+  const [listaCompleta, setListaCompleta] = useState([]);
+  const [Error, setError] = useState  (false);
+
+  useEffect(() => {
+    const getCarros = async() => {
+      try{
+        const objeto = await fetch('https://localhost:3000/api');
+        const data = await objeto.json();
+        setListaDeCarros(data);
+        setListaCompleta(data);
+      }
+      catch{
+        setError(true);
+      }
+    }
+    getCarros();
+  },[]);
+
+  if (Error == true){
+    return <ErrorGetFetch/>
+  }
+
+if (listaCompleta[0] == null){
+  return
+}
+
+return (
+  <main className={styles.main}>
+    {listaDeCarros.map((carro) => (
+      <div className={styles.card} key={carro.id}>
+        {<div className={styles.titulo}>{carro.titulo.slice(5, 50)}...</div>}
+
+        <Image className={styles.img} width={300} height={400} src={carro.linkImg} />
+
+        {<div className={styles.propriedades}>{carro.modelo}</div>}
+
+        {<div className={styles.propriedades}><strong>Fabricante:</strong> {carro.fabricante}</div>}
+
+        {<div className={styles.propriedades}><strong>Ano</strong> {carro.ano}</div>}
+
+        {<div className={styles.ultimaProp}><strong>Cor</strong> {carro.cor}</div>}
+
+        <Link href={'/carros/' + carro.id}>
+        <button className={styles.button}>Saiba mais</button>
+        </Link>
+
+      </div>
+    ))}
+  </main>
+);
 }
